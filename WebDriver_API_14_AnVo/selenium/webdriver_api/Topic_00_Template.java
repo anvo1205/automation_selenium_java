d428da9c4ece381035944d276c2ad08a832253a9
anvo1205 <39280916+anvo1205@users.noreply.github.com>
2019-12-31 12:22:49 -0500

    Add template

---
+++ WebDriver_API_14_AnVo/selenium/webdriver_api/Topic_00_Template.java
@@ -0,0 +1,40 @@
+package webdriver_api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class Topic_00_Template {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", "C:\\An\\software\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/v4");
+	}
+
+	@Test
+	public void TC_01_CheckUrl() {
+		
+	}
+
+	@Test
+	public void TC_02_CheckTitle() {
+	
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
