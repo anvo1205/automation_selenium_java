e5b6f6be83fc838ff5508c63f7cd3a24103a0096
anvo1205 <39280916+anvo1205@users.noreply.github.com>
2019-12-31 12:00:22 -0500

    First commit

---
+++ WebDriver_API_14_AnVo/selenium/webdriver_api/Topic_01_Check_Environment.java
@@ -0,0 +1,43 @@
+package webdriver_api;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic_01_Check_Environment {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", "C:\\An\\software\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://demo.guru99.com/v4");
+	}
+
+	@Test
+	public void TC_01_CheckUrl() {
+		String loginPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginPageUrl, "http://demo.guru99.com/v4/");
+	}
+
+	@Test
+	public void TC_02_CheckTitle() {
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Guru99 Bank Home Page");
+		
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+}
